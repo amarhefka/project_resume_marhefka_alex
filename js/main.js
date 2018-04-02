94719b280948824a1ad8e4984b3c66b533758412
Alex <alex.marhefka@gmail.com>
2018-04-01 21:23:52 -0400

    Add the jQuery.scrolling plugin

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
 $(document).ready(function () {
-
+    // use jQuery.scrolling plugin to reveal articles as the user scrolls through the page
+    $('article').scrolling({ offsetTop: -100 });
+    $('article').first().siblings().animate({opacity: 0}, 0);
+    $('article').on('scrollin', function () {
+        $(this).animate({opacity: 1}, 600);
+    });
 });
